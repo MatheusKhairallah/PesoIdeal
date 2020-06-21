73f37fb57996e1ec5be55ee55fd9f8b1c31db87e
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:53:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PesoIdeal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua altura em M: ");
+            double altura=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite seu sexo Masculino / Feminino: ");
+            string sexo=Console.ReadLine();
+
+            double resultado1=(altura * 72.7) - 58.0;
+            double resultado2=(altura * 62.1) - 44.7;
+
+            Console.Clear();
+
+            if(sexo=="Masculino")
+            Console.WriteLine($"Seu peso ideal é {resultado1}kg");
+            else if(sexo=="Feminino")
+            Console.WriteLine($"Seu peso ideal é {resultado2}kg");
+
+        }
+    }
+}
